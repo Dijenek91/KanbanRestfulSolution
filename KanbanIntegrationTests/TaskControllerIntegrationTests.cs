c2e95f82d496f8d55330435384837c2eca071ae0
dijenek91 <bratic.goran@gmail.com>
2026-04-05 10:56:58 +0200

    Integraiton tests - TaskController - added for getById and Create Task

--- KanbanIntegrationTests/TaskControllerIntegrationTests.cs
+++ KanbanIntegrationTests/TaskControllerIntegrationTests.cs
@@ -1,5 +1,6 @@
 ﻿using KanbanInfrastructure.DAL;
 using KanbanIntegrationTests.CustomTestSupportItems;
+using KanbanModel.DTOs.RequestDTOs;
 using KanbanModel.DTOs.ResponseDTOs;
 using KanbanModel.ModelClasses;
 using Microsoft.Extensions.DependencyInjection;
@@ -35,10 +36,6 @@ namespace KanbanIntegrationTests
         [SetUp]
         public void Setup()
         {
-            //var token = TestJwtHelper.GenerateToken();
-            //_client.DefaultRequestHeaders.Authorization =
-            //    new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
-
             var token = TestJwtHelper.GetJwtTokenAsync(_client);
             _client.DefaultRequestHeaders.Authorization =
                 new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token.Result);
@@ -105,6 +102,110 @@ namespace KanbanIntegrationTests
             Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
             var taskReponse = response.Content.ReadFromJsonAsync<PagedResultKanbanTasksResponse<KanbanTaskResponse>>().Result;
             Assert.That(taskReponse.Items.Count, Is.EqualTo(0));
+        }
+
+        #endregion
+
+        #region GetById task
+        [Test]
+        public async Task GetById_ReturnsTask()
+        {
+            List<KanbanTask> kanbanTasks = new List<KanbanTask>()
+            {
+                new KanbanTask
+                {
+                    Id = 0,
+                    Name = "Task1",
+                    Description = "Description1",
+                    Status = StatusEnum.ToDo
+                },
+                new KanbanTask
+                {
+                    Id = 1,
+                    Name = "Task2",
+                    Description = "Description2",
+                    Status = StatusEnum.ToDo
+                }
+            };
+
+            await SeedAsync(kanbanTasks);
+            
+            var response = await _client.GetAsync("/api/tasks/1");
+
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+            var taskReponse = response.Content.ReadFromJsonAsync<KanbanTaskResponse>().Result;
+            Assert.That(taskReponse.Id, Is.EqualTo(1));
+        }
+
+        [Test]
+        public async Task GetById_ReturnsNoTask()
+        {
+            List<KanbanTask> kanbanTasks = new List<KanbanTask>()
+            {
+                new KanbanTask
+                {
+                    Name = "Task1",
+                    Description = "Description1",
+                    Status = StatusEnum.ToDo
+                },
+                new KanbanTask
+                {
+                    Name = "Task2",
+                    Description = "Description2",
+                    Status = StatusEnum.ToDo
+                }
+            };
+
+            await SeedAsync(kanbanTasks);
+
+            var response = await _client.GetAsync("/api/tasks/5");
+
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.NotFound));
+            var taskReponse = response.Content.ReadFromJsonAsync<PagedResultKanbanTasksResponse<KanbanTaskResponse>>().Result;
+            Assert.That(taskReponse.Items, Is.Null);            
+        }
+        #endregion
+
+        #region Create
+
+        [Test]
+        public async Task CreateTask_Returns201Created()
+        {
+            await SeedEmptyAsync();
+
+            var request = new CreateKanbanTaskRequest()
+            {
+                Name = "new Task",
+                Description = "new descr",
+                PriorityEnum = PriorityEnum.Medium,
+                Size = 3,
+                Status = StatusEnum.ToDo
+            };
+
+            var response = await _client.PostAsJsonAsync("/api/tasks", request);
+
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.Created));
+            var taskReponse = response.Content.ReadFromJsonAsync<KanbanTaskResponse>().Result;
+            Assert.That(taskReponse.Name, Is.EqualTo(request.Name));
+        }
+
+        [Test]
+        public async Task CreateTask_BadRequest()
+        {
+            await SeedEmptyAsync();
+
+            var request = new CreateKanbanTaskRequest()
+            {
+                Name = "new Task",
+                Description = "new descr",
+                PriorityEnum = PriorityEnum.Medium,
+                Size = 0, //invalid model -> throws restriction
+                Status = StatusEnum.ToDo
+            };
+
+            var response = await _client.PostAsJsonAsync("/api/tasks", request);
+
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.BadRequest));
         }
 
         #endregion
